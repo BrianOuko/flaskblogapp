d6b68e0228cde57d6418c83c3f88e8d939f85aa5
Brian <oukobrian001@gmail.com>
2024-07-12 11:58:34 +0300

    forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask (__name__)
 
+#setting a secret key to prevent modifying cookies, cross-siterequests and forgery attacks
+app.config['SECRET_KEY']='0346d7a54607eab673efb17f6eab633fc3c8cafcc2d53cec5ae80379947e0482'
 posts= [
     {
         'author': 'Brian O',
@@ -22,6 +25,16 @@ def home ():
 @app.route("/about")
 def about ():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register ():
+    form=RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login ():
+    form=LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__== '__main__':
     app.run(debug=True)
