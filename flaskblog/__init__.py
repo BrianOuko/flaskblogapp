205a32765b7c6cca6c66641ba9942b38fd664c50
Brian <oukobrian001@gmail.com>
2024-08-02 09:09:54 -0300

    authenticate users, enable logins & logouts

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy #alchemy is an ORM for .py
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask (__name__)
 app.config['SECRET_KEY']='0346d7a54607eab673efb17f6eab633fc3c8cafcc2d53cec5ae80379947e0482'#secret key above generated from python using import secrets|secrets.token_hex(32).Setting a secret key to prevent modifying cookies, cross-siterequests and forgery attacks
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db=SQLAlchemy(app) #creating a db instance
+bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message_category='info'
 
-from flaskblog import routes # we import routes after initializing the app because the routes file imports the app variable itself
+from flaskblog import routes #we import routes after initializing the app because the routes file imports the app variable itself
--- flaskblog/terminal.py
+++ flaskblog/terminal.py
@@ -1,8 +1,51 @@
-#python code used on the terminal
+#python code used on the terminal -starting from database init onwards
 #changing the structure to packages
 python3
-from flaskblog.models import User, Post
-from flaskblog import app, db
-app.app_context().push()
-db.create_all()
-User.query.all()
+>>> from flaskblog.models import User, Post
+>>> from flaskblog import app, db
+>>> app.app_context().push()
+>>> db.create_all()
+>>> User.query.all()
+
+pip install flask-bcrypt
+
+>>> python3
+>>> from flask_bcrypt import Bcrypt
+>>> bycrypt_pw=Bcrypt()
+>>> bycrypt_pw.generate_password_hash('testing') #result =b'$2b$12$r7ultaMlmzmcPF4LVWnzduSxo9U3nljQDE1tYm47u6jhKpOhun8je'
+>>> bycrypt_pw.generate_password_hash('testing').decode('utf-8')# result='$2b$12$DpqSj7YXcKTg0P89e0j9cepZafKqNQjYLhDF6DmubWiaqsf.1BoW2'
+
+>>> hashed_pw=bycrypt_pw.generate_password_hash('testing').decode('utf-8')# save hashed pw as a variable
+>>> bycrypt_pw.check_password_hash(hashed_pw, 'colour')
+False
+>>> bycrypt_pw.check_password_hash(hashed_pw, 'testing')
+True
+>>> 
+
+#Checking out our db
+# user is first added to the db using register function on routes
+#def register ():
+    #form=RegistrationForm()
+    #if form.validate_on_submit():
+        hashed_pw=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user=User(username=form.username.data, email=form.email.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+
+#on the terminal..
+>>> from flaskblog import app, db
+>>> app.app_context().push()
+>>> from flaskblog.models import User
+>>> user=User.query.first()
+>>> user
+User('briano',briano@hotmail.com'. 'default.jpg')
+>>> user.password
+'$2b$12$QonMK26ZmE1uwjLYKjJ5FOIi2Ax2B1qk/MBQ4XoWE2d6Q4B4SCwOO'
+
+#to prevent adding the same user twice import and use ValidationError and validate_field fn from wtf forms on forms.py
+
+#to create a login system after the registration system use the Flask login extension to manager user sessions
+#on the terminal..
+>>> pip install flask-login
+
+#to restrict access to specific routes
